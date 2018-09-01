ba32406833c1a566ae6cc03d0138b90b822ed773
ofermoscovich <omoscovich@gmail.com>
2018-09-01 08:27:33 +0300

    reverting last change

--- src/Kefel/Main.java
+++ src/Kefel/Main.java
@@ -8,46 +8,47 @@ import java.util.Date;
  * פרויקט לוח הכפל של הדרי
  */
 public class Main {
-	
+
 	private static final int MAXTESTS = 5; // מקסימום מבחנים
 	private static final int MAXQUESTIONS = 8; // מקסמום תרגילים למבחן
 	private static final int MAXTRIES = 4;  // מקסימום ניסיונות להשיב נכון
 	private static final int SHOWTABLE = 0; // האם להציג את טבלת לוח הכפל: 1 כן; 2 לא
-												// מספר שמאלי
+	// מספר שמאלי
 	private static final int MINNUMBER1 = 2; // מספר אקראי מינימלי בתרגיל
 	private static final int MAXNUMBER1 = 9; // מספר אקראי מקסימלי בתרגיל
-												// מספר ימני
+	// מספר ימני
 	private static final int MINNUMBER2 = 2; // מספר אקראי מינימלי בתרגיל
 	private static final int MAXNUMBER2 = 9; // מספר אקראי מקסימלי בתרגיל
-	
+
 	private static final int SHUFFLE = 1;   // לערבב תרגילים: 1 כן;0 לא
-					  // ציור לוח הכפל
+	// ציור לוח הכפל
 	public static int y, //  מונה שורות
-					  x, // מונה עמודות
-					  
-					  // משחק הכפולות
-					  k, // משחק הכפולות - מונה 10 תרגילים
-					  num1,num2, // קבלת שני מספרים אקראיים למכפלה
-					  input, // משחק הכפולות - קבלת קלט מהמשתמש
-					  tziun, // צובר ציון ברמת מבחן אחד
-					  g, // מונה 10 מבחנים
-					  tziunAll, //  צובר ציון לכל המבחנים שנעשו
-					  l, // מונה השהייה בין הגרלת מספרים לכפולות
-					  i, // מונה מחזוריות הזדמנויות להשיב ולתקן תשובה לא נכונה
-					  s, // מונה מספר תרגילים עבור אתחול המשחק בתרגילים
-					  minNumberLeft, // סדר המספרים האמיתי שירוץ על לולאת אתחול התרגילים
-					  maxNumberLeft, // ההחלפה נועדה למקרה שמטריצת לוח הכפל שנבחרה אינה
-					  minNumberRight,// שווה ויש טווח רוחב מספרים גדול מטווח אורך
-					  maxNumberRight,// במקרה זה, כדי לתמוך בהעדר חזרות על תרגילים, נעמיד את הטבלה בהחלפת סדר המספרים בתרגיל
-					  numOfTargilim,  // מונה כמות התרגילים באתחול תרגילים (מקסימום מספר תרגילים למבחן * מספר המבחנים)
-					  regularLoop;  // באתחול תרגילים אינדיקציה להחלפת מקום המספרים בתרגיל 
+			x, // מונה עמודות
+
+	// משחק הכפולות
+	k, // משחק הכפולות - מונה 10 תרגילים
+			num1,num2, // קבלת שני מספרים אקראיים למכפלה
+			input, // משחק הכפולות - קבלת קלט מהמשתמש
+			tziun, // צובר ציון ברמת מבחן אחד
+			g, // מונה 10 מבחנים
+			tziunAll, //  צובר ציון לכל המבחנים שנעשו
+			l, // מונה השהייה בין הגרלת מספרים לכפולות
+			i, // מונה מחזוריות הזדמנויות להשיב ולתקן תשובה לא נכונה
+			s, // מונה מספר תרגילים עבור אתחול המשחק בתרגילים
+			minNumberLeft, // סדר המספרים האמיתי שירוץ על לולאת אתחול התרגילים
+			maxNumberLeft, // ההחלפה נועדה למקרה שמטריצת לוח הכפל שנבחרה אינה
+			minNumberRight,// שווה ויש טווח רוחב מספרים גדול מטווח אורך
+			maxNumberRight,// במקרה זה, כדי לתמוך בהעדר חזרות על תרגילים, נעמיד את הטבלה בהחלפת סדר המספרים בתרגיל
+			numOfTargilim,  // מונה כמות התרגילים באתחול תרגילים (מקסימום מספר תרגילים למבחן * מספר המבחנים)
+			regularLoop;  // באתחול תרגילים אינדיקציה להחלפת מקום המספרים בתרגיל
 	public static long secondsDiff, // הפרש שניות עד מתן תשובה כלשהי או תשובה נכונה
-					  secondsTest,  // משך הזמן המצטבר שלקח להשיב על מבחן אחד
-					  secondsTestAll; // משך הזמן המצטבר שלקח להשיב על כל המבחנים
+			secondsTest,  // משך הזמן המצטבר שלקח להשיב על מבחן אחד
+			secondsTestAll; // משך הזמן המצטבר שלקח להשיב על כל המבחנים
 
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		ArrayList<Integer[]> targil = new ArrayList<Integer[]>();
+
 
 		//=========== ציור לוח הכפל ===========
 		// הצגת כותרת
@@ -83,10 +84,9 @@ public class Main {
 				// המשך לשורה הבאה
 			}
 		}
-		
+
 		//=========== משחק תרגילים ===========
 		// ----  רמת כל המשחק ואתחול ----
-
 		// החלף את צדדי המספרים לצורך אתחול מחסנית התרגילים
 		// הדבר נועד לתמוך בלוגיקת מניעת חזרות של תרגילים במחסנית
 		// לשם כך נבדוק אם המטריצה של לוח הכפל הנבחר ע"י המשתמש
@@ -96,7 +96,7 @@ public class Main {
 		// כלומר נעמיד את מלבן המטריצה כדי לכסות את הטווח הארוך בין הרוחב והאורך
 		// וכך ניצור כיסוי מלא של תרגילים תוך שמירת עיקרון מניעת חזרות
 		// ובהתאם לבחירת המשתמש בכמות מבחנים, מספר תריגילים במבחן והטווחים של המספרים
-		if(MAXNUMBER1 - MINNUMBER1 >= MAXNUMBER2 - MINNUMBER2) {
+		if(MAXNUMBER1 - MINNUMBER1 > MAXNUMBER2 - MINNUMBER2) {
 			minNumberLeft = MINNUMBER1;
 			maxNumberLeft = MAXNUMBER1;
 			minNumberRight = MINNUMBER2;
@@ -107,7 +107,6 @@ public class Main {
 			minNumberRight = MINNUMBER1;
 			maxNumberRight = MAXNUMBER1;
 		}
-
 		// המטרה של אתחול התרגילים מראש הוא לבצע בקרה על כל התרגילים במכל המבחנים
 		// כדי למנוע חזרה על תרגילים שהיו באותו מבחן ובמבחנים אחרים באותו סט
 		// מלא מחסנית בתרגילים כמספר כמות תרגילים בכל מבחן ומספר המבחנים
@@ -122,21 +121,21 @@ public class Main {
 					if (regularLoop==0) {
 						// הצב במחסנית את הכפולה (שני המספרים) ואת התוצאה
 						targil.add(new Integer[] {y, x, x*y});
-						
+
 						// אם אינדיקציה לסיבוב חדש (שני וכו) של אתחול
-						// אז החלף בין המספרים ודחוף מספר ראשון לימין ומספר שני לשמאל 
+						// אז החלף בין המספרים ודחוף מספר ראשון לימין ומספר שני לשמאל
 					} else if (regularLoop==1) {
 						// הצב במחסנית את הכפולה (שני המספרים) ואת התוצאה
 						targil.add(new Integer[] {x, y, x*y});
 					}
 					// קדם/עדכן את מספר התרגילים שהוכנסו למחסנית
 					numOfTargilim++ ;
-					// אם תוצאת הכפולה שווה לכפולת מספר עמודה באותו מספר עמודה אז סיים את השורה 
+					// אם תוצאת הכפולה שווה לכפולת מספר עמודה באותו מספר עמודה אז סיים את השורה
 					// כדי למנוע חזרה על תרגילים בהחלפת צדדי מספרי הכפולה
 					if(x*y==y*y) break;
-					if(numOfTargilim == ((MAXNUMBER1 - MINNUMBER1+1) * (MAXNUMBER2 - MINNUMBER2+1))) break;
+					if(numOfTargilim == MAXTESTS*MAXQUESTIONS) break;
 				}
-				if(numOfTargilim == ((MAXNUMBER1 - MINNUMBER1+1) * (MAXNUMBER2 - MINNUMBER2+1))) break;
+				if(numOfTargilim == MAXTESTS*MAXQUESTIONS) break;
 			}
 			// במידה והסתיים אתחול כל התרגילים האפשריים לטווח הנתון ונשאר מקומות ריקים במכסה,
 			// כלומר אם טרם מולאה מכסת המחסנית) החל חזרות תוך החלפת מקומות המספרים
@@ -146,18 +145,17 @@ public class Main {
 				regularLoop = 0;
 			}
 			// המשך באתחול מחסנית התרגילים כל עוד לא הגעת למספר כולל מקסימלי של תרגילים לכל המבחנים
-		} while(numOfTargilim < ((MAXNUMBER1 - MINNUMBER1+1) * (MAXNUMBER2 - MINNUMBER2+1)) || (numOfTargilim < MAXQUESTIONS*MAXTESTS));
+		} while(numOfTargilim < MAXTESTS*MAXQUESTIONS );
 
-//		for (int e = 0; (e < ((MAXNUMBER1 - MINNUMBER1+1) * (MAXNUMBER2 - MINNUMBER2+1)) || (numOfTargilim < MAXQUESTIONS*MAXTESTS));e++) {
+//		for (int e = 0; e<= MAXTESTS*MAXQUESTIONS;e++) {
 //			//int bbb = targil.getFirst()[0] ;// + " * " + targil.getFirst() + " = " + targil.getFirst());
 //			System.out.println("(" + e + ")   " + targil.get(e)[0] + " * " + targil.get(e)[1] + " = " + targil.get(e)[2]);
 //		}
-
 		// תערבב תרגילים אם ההגדרה היא 1
 		if(SHUFFLE == 1) Collections.shuffle(targil);
 
 		// --- רוץ ברמת כל המבחנים (מספר מבחנים לסט אחד)  ----
-		do {               
+		do {
 			// אפס משתנים
 			tziun = 0;
 			// כותרת מבחן מספר __
@@ -167,43 +165,43 @@ public class Main {
 			System.out.println("מבחן מספר " + (g+1) + " מתוך " + MAXTESTS);
 			System.out.println("__________________");
 
-			// --- רוץ ברמת מבחן אחד (כמות תרגילים למבחן אחד) ---- 
+			// --- רוץ ברמת מבחן אחד (כמות תרגילים למבחן אחד) ----
 			for (k = 1; k <= MAXQUESTIONS; k++){
 				// הבא מהמחסנית שני מספרים אקראיים
 				// מספר שמאלי
 				num1 = targil.get(0)[0];
-				
+
 				// מספר ימני
 				num2 = targil.get(0)[1] ;
-				
+
 				// הסר תרגיל מהרשימה למניעת חזרתו
-				targil.remove(0);	
-			    
+				targil.remove(0);
+
 				// מדוד זמן התחלה
 				Date now1 = new Date();
 
 				// לולאת מחזוריות הזדמנויות להשיב במקרה ותשובה אינה נכונה
 				// אם התשובה נכונה, הלולאה תפסיק ונעבור לתרגיל הבא
-			    // --- רמת שאלה במבחן ---- 
+				// --- רמת שאלה במבחן ----
 				for (i = 1; i <= MAXTRIES; i++){
 					// הדפס תרגיל
 					System.out.print("(" + k + ")   " + num1 + " x " + num2 + " = ");
-					
+
 					// קבל קלט תשובה מהמשתמש
 					boolean isInputCorrect = true;
-				    do{
-				    	// בדיקה לוודא שיוקלד רק מספר
-				        if(console.hasNextInt()){ 
-				        	input = console.nextInt();
-				        	isInputCorrect = true;
-				        }
-				        else{
-				            System.out.print("  הקלדה שגויה, הקלידי שוב ");
-				            isInputCorrect = false;
-				            console.next();
-				        }
-				    } while(isInputCorrect == false);
-					
+					do{
+						// בדיקה לוודא שיוקלד רק מספר
+						if(console.hasNextInt()){
+							input = console.nextInt();
+							isInputCorrect = true;
+						}
+						else{
+							System.out.print("  הקלדה שגויה, הקלידי שוב ");
+							isInputCorrect = false;
+							console.next();
+						}
+					} while(isInputCorrect == false);
+
 					// בצע בדיקת נכונות התשובה והצג מסקנה
 					// אם התשובה נכונה - צא מהלולאה והמשך לתרגיל הבא
 					if (input==num1*num2){
@@ -253,14 +251,14 @@ public class Main {
 				secondsTest += secondsDiff ;
 			}
 			// צבור את שניות מתן התשובות של המבחן לצובר כל המבחנים
-			secondsTestAll += secondsTest ;			
-			
+			secondsTestAll += secondsTest ;
+
 			// הצג ציון סופי של בחינה אחת
-			
+
 			System.out.println("___________________________________________");
 			System.out.println("הציון שלך הוא: " + tziun * 100 / MAXQUESTIONS + "    ");
 			System.out.println("הממוצע לתרגיל: " + secondsTest/MAXQUESTIONS + " שניות");
-			System.out.println("לקח לך להשיב: " + secondsTest + " שניות");			
+			System.out.println("לקח לך להשיב: " + secondsTest + " שניות");
 			// תנאים לפרגון או המלצה לשיפור
 			if (tziun * 100 / MAXQUESTIONS > 90){
 				// אם הציון מעל 90 ציין זאת
@@ -269,42 +267,42 @@ public class Main {
 					// אם זמן תרגיל ממוצע פחות מ 10 שניות ציין זאת
 					System.out.println("כל הכבוד הדרי - המלכה  !!! פחות מ 9 שניות לתרגיל") ;
 				}
-			} else if (tziun * 100 / MAXQUESTIONS < 60){ 
+			} else if (tziun * 100 / MAXQUESTIONS < 60){
 				// אם הציון מתחת 70 ציין זאת
-				System.out.println("הדרי, מומלץ להוסיף ולתרגל") ;			
+				System.out.println("הדרי, מומלץ להוסיף ולתרגל") ;
 			}
 
 			// מונה מבחנים
 			g++ ;
-			
+
 			// הצג נתונים סופיים של כל הבחינות עד עתה
-			
+
 			System.out.println("___________________________________________");
 			if(g > 1) { // מקרה קצה: הצג ציון ממוצע בכל המבחנים רק אם זה לא המבחן הראשון
-				System.out.println("ציונך הממוצע בכל המבחנים עד עתה הוא: " + tziunAll * 100 / MAXQUESTIONS / g ) ; 
-				System.out.println("זמנך הממוצע לפתירת תרגיל: " + secondsTestAll / MAXQUESTIONS / g + " שניות") ; 
+				System.out.println("ציונך הממוצע בכל המבחנים עד עתה הוא: " + tziunAll * 100 / MAXQUESTIONS / g ) ;
+				System.out.println("זמנך הממוצע לפתירת תרגיל: " + secondsTestAll / MAXQUESTIONS / g + " שניות") ;
 				System.out.println();
 			}
 			if(g < MAXTESTS) { // מקרה קצה: שאל אם להמשיך למבחן הבא ככל שטרם הגיע המבחן האחרון
 				// שאלה למשתמש האם ברצונו להמשיך למבחן נוסף
 				System.out.println("האם ברצונך להמשיך למבחן הבא?");
 				System.out.println("להמשך הקש 1; לסיום הקש 0") ;
-				
+
 				// קבל קלט תשובה מהמשתמש (כפילות לפונקציה)
 				boolean isInputCorrect = true;
-			    do{
-			    	// בדיקה לוודא שיוקלד רק מספר
-			        if(console.hasNextInt()){ 
-			        	input = console.nextInt();
-			        	isInputCorrect = true;
-			        }
-			        else{
-			            System.out.print("  הקלדה שגויה, הקלידי שוב ");
-			            isInputCorrect = false;
-			            console.next();
-			        }
-			    } while(isInputCorrect == false);
-				// אפס צובר כל המבחנים, את צובר כל זמן במחינה ואת צובר כל זמן 10 הבחינות,  
+				do{
+					// בדיקה לוודא שיוקלד רק מספר
+					if(console.hasNextInt()){
+						input = console.nextInt();
+						isInputCorrect = true;
+					}
+					else{
+						System.out.print("  הקלדה שגויה, הקלידי שוב ");
+						isInputCorrect = false;
+						console.next();
+					}
+				} while(isInputCorrect == false);
+				// אפס צובר כל המבחנים, את צובר כל זמן במחינה ואת צובר כל זמן 10 הבחינות,
 				secondsTest = 0;
 				//tziunAll = 0;
 				//secondsTestAll = 0; // לא חובה לאפס כי זה נתון סופי
@@ -316,7 +314,7 @@ public class Main {
 				System.out.println("תקנה לי פלאפון עכשיו!!! תודה אבא, יש, יש, יש") ;
 				System.out.println("===============================================") ;
 			}
-		// בצע לולאה של 10 מבחנים או הקשת 0 ליציאה	
+			// בצע לולאה של 10 מבחנים או הקשת 0 ליציאה
 		} while(g < MAXTESTS && !(input==0));
 		console.close();
 	}
